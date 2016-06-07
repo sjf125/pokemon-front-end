3c92412856722ea3b05728338c37cff252ef7915
sjf125 <sjf125@gmail.com>
2016-06-07 09:56:38 -0400

    Add pokemon model and route
    Add in attrs to model

---
+++ app/pokemon/model.js
@@ -0,0 +1,18 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  name: attr('string'),
+  hidden: attr('boolean'),
+  items: hasMany('item'),
+  type1: attr('string'),
+  type2: attr('string'),
+  hp: attr('number'),
+  attack: attr('number'),
+  defense: attr('number'),
+  spatk: attr('number'),
+  spdef: attr('number'),
+  speed: attr('number'),
+  image: attr('string')
+});
