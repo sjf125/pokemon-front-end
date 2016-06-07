3bc805faf3c53de17f3d79bcdb6d5d645bd92f99
sjf125 <sjf125@gmail.com>
2016-06-07 11:14:45 -0400

    Change pokemon model
    to output correct capitalizations

--- app/pokemon/model.js
+++ app/pokemon/model.js
@@ -1,12 +1,21 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { hasMany } from 'ember-data/relationships';
+import Ember from 'ember';
+// import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr('string'),
-  hidden: attr('boolean'),
+  capname: Ember.computed('name', function() {
+    return this.get('name').capitalize();
+  }),
   type1: attr('string'),
   type2: attr('string'),
+  typeone: Ember.computed('type1', function() {
+    return this.get('type1').toUpperCase();
+  }),
+  typetwo: Ember.computed('type2', function() {
+    return this.get('type2').toUpperCase();
+  }),
   hp: attr('number'),
   attack: attr('number'),
   defense: attr('number'),
