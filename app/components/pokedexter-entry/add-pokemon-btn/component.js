7e5ff6192edc90ce4537042a9682d62dc07c52dd
sjf125 <sjf125@gmail.com>
2016-06-08 17:43:52 -0400

    Successfullt get data
    from add-to-poketeam button componenet
    Just need to figure out how to get it into local storage

--- app/components/pokedexter-entry/add-pokemon-btn/component.js
+++ app/components/pokedexter-entry/add-pokemon-btn/component.js
@@ -2,12 +2,14 @@ import Ember from 'ember';
 import { storageFor } from 'ember-local-storage';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   poketeam: storageFor('poketeam'),
 
   actions: {
     addPokemon (pokemon) {
-      let log = this.get(pokemon);
+      let log = this.get('pokemon');
       console.log(log);
+
       // let poketeam = this.get('poketeam').get('id', result.user.id);
       // this.sendAction('addPokemon', pokemon);
     }
