168592c31c8ecd68c34931bdb5b0e96d80f6dee9
sjf125 <sjf125@gmail.com>
2016-06-10 09:17:53 -0400

    Change project name to pokedexter

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'pokedexter',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
