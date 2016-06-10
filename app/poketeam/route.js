badd6a03e5e77f99ae8c24379e68aeef2b769608
sjf125 <sjf125@gmail.com>
2016-06-10 14:38:03 -0400

    Fix poketeam display

--- app/poketeam/route.js
+++ app/poketeam/route.js
@@ -8,12 +8,16 @@ export default Ember.Route.extend({
     return store.queryRecord('poketeam', {})
     .then(function(data) {
       console.log(data.get('slot1'));
-    let poke = data.get('slot1');
-    store.findRecord('pokemon', poke)
-    .then(function(data) {
-      console.log(data.get('name'));
-      return data;
-    });
+      let poke = data.get('slot1');
+      if (poke) {
+        return store.findRecord('pokemon', poke)
+        .then(function(data) {
+          console.log(data.get('name'));
+          return data;
+        });
+      } else {
+        return Promise.resolve({});
+      }
     });
   }
 });
